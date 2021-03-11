6d2c3f659c8ab74fc48824e3746be7df7a1de8e1
Oleg Plichko <Oleg_Plichko@epam.com>
2021-03-11 20:27:35 +0300

    Add spotify

--- script.py
+++ script.py
@@ -6,11 +6,19 @@ from time import sleep
 
 import requests
 import yaml
+import base64
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 TELEGRAM_API_URL = f'https://api.telegram.org/bot{BOT_TOKEN}'
 SCHEDULE_CHAT_ID = os.environ.get('SCHEDULE_CHAT_ID', 0)
 CHAT_IDS = [SCHEDULE_CHAT_ID]
+SPOTIFY_ACCESS_TOKEN = None
+SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
+SPOTIFY_REFRESH_TOKEN = os.environ.get('SPOTIFY_REFRESH_TOKEN')
+CLIENT_CREDS = f"{SPOTIFY_CLIENT_ID}:{SPOTIFY_CLIENT_SECRET}"
+CLIENT_CREDS_B64 = base64.b64encode(CLIENT_CREDS.encode())
+
 mode = os.environ.get('MODE', 'debug')
 DEBUG = mode == 'debug'
 
@@ -55,13 +63,12 @@ def settime(hours_minutes_str=None, **kwargs):
     return result
 
 
-if __name__ == '__main__':
-    with open('config.yaml') as f:
+def run(config_path):
+    with open(config_path) as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
-
     skipped = True
-    weekday = datetime.today().weekday()
-    for item in config['data']:
+    n = len(config['data']) - 1
+    for index, item in enumerate(config['data']):
         now = datetime.now()
         now = now.replace(microsecond=0)
         end_time = datetime.now()
@@ -71,12 +78,64 @@ if __name__ == '__main__':
             emoji = random.choice(item['emoji']) if isinstance(item['emoji'], list) else item['emoji']
             text = f"{emoji} <b>{item['start']} - {item['stop']}</b> <i>{item['description']}</i>"
             send_message(text)
+            if item['status'] == 'B' or item['status'] == 'LB':
+                access_token = refresh_spotify_token()
+                play_spotify(access_token)
         total_seconds = (end_time - now).total_seconds()
         delay = int(total_seconds)
         skipped = delay < 0
         if skipped:
             logger.debug('skipped %s', item)
-            continue
         else:
             logger.debug('now: %s delay: %s alert time: %s', now, delay, now + timedelta(seconds=delay))
             wait(delay)
+        if index == n:
+            for i in range(15):
+                refresh_spotify_token()
+                sleep(60 * 60)
+
+
+def set_volume(volume, access_token):
+    requests.put(
+        f"https://api.spotify.com/v1/me/player/volume?volume_percent={volume}",
+        headers={
+            "Authorization": f"Bearer {access_token}"
+        }
+    )
+
+
+def refresh_spotify_token():
+    response = requests.post(
+        "https://accounts.spotify.com/api/token",
+        data={
+            'grant_type': 'refresh_token',
+            'refresh_token': SPOTIFY_REFRESH_TOKEN,
+        },
+        headers={
+            "Authorization": f"Basic {CLIENT_CREDS_B64.decode()}"
+        }
+    )
+    print(response.json())
+    result = response.json()['access_token']
+    with open('token', 'w') as f:
+        f.write(result)
+    return result
+
+
+def play_spotify(access_token):
+    volume = 10
+    set_volume(volume, access_token)
+    requests.put(
+        "https://api.spotify.com/v1/me/player/play",
+        headers={
+            "Authorization": f"Bearer {access_token}"
+        }
+    )
+    for i in range(10):
+        volume += 5
+        set_volume(volume, access_token)
+        sleep(5)
+
+
+if __name__ == '__main__':
+    run('config.yaml')
